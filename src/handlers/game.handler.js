69c8cae778cc1e7d07ceab5e19e2bb8365b127e7
hjy2007 <wjddud2007@gmail.com>
2024-10-14 21:14:22 +0900

    tutor_project_ctrl_c+v
    그냥 튜터님 파일 붙여넣기로 작성했습니다

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,9 +1,12 @@
 import { getGameAssets } from '../init/assets.js';
 import { clearStage, getStage, setStage } from '../models/stage.model.js';
+import calculateTotalScore from '../utils/calculateTotalScore.js';
+import { getUserItems, initializeItems } from '../models/item.model.js';
 
 export const gameStart = (uuid, payload) => {
   const { stages } = getGameAssets();
   clearStage(uuid);
+  initializeItems(uuid);
   setStage(uuid, stages.data[0].id, payload.timestamp);
   console.log('Stage:', getStage(uuid));
 
@@ -14,29 +17,18 @@ export const gameEnd = (uuid, payload) => {
   // 클라이언트에서 받은 게임 종료 시 타임스탬프와 총 점수
   const { timestamp: gameEndTime, score } = payload;
   const stages = getStage(uuid);
+  const userItems = getUserItems(uuid);
 
   if (!stages.length) {
     return { status: 'fail', message: 'No stages found for user' };
   }
 
-  // 각 스테이지의 지속 시간을 계산하여 총 점수 계산
-  let totalScore = 0;
-  stages.forEach((stage, index) => {
-    let stageEndTime;
-    if (index === stages.length - 1) {
-      // 마지막 스테이지의 경우 종료 시간이 게임의 종료 시간
-      stageEndTime = gameEndTime;
-    } else {
-      // 다음 스테이지의 시작 시간을 현재 스테이지의 종료 시간으로 사용
-      stageEndTime = stages[index + 1].timestamp;
-    }
-    const stageDuration = (stageEndTime - stage.timestamp) / 1000; // 스테이지 지속 시간 (초 단위)
-    totalScore += stageDuration; // 1초당 1점
-  });
+  // 각 요소를 총합하여 총 점수 계산
+  const totalScore = calculateTotalScore(stages, gameEndTime, false, userItems);
 
   // 점수와 타임스탬프 검증 (예: 클라이언트가 보낸 총점과 계산된 총점 비교)
-  // 오차범위 5
-  if (Math.abs(score - totalScore) > 5) {
+  // 오차범위 1
+  if (Math.abs(totalScore - score) > 1) {
     return { status: 'fail', message: 'Score verification failed' };
   }
 
